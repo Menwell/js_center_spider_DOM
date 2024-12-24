dab332125437b4475921dc0cb0c01565b5391f6e
Nikita <onishenkonikitos@gmail.com>
2024-12-24 06:02:15 +0100

    make build

---
+++ index.3c9ec03e.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=document.querySelector(".spider"),c=e.getBoundingClientRect(),n=t.getBoundingClientRect();t.style.top="".concat((c.height-n.height)/2,"px"),t.style.left="".concat((c.width-n.width)/2,"px");
+//# sourceMappingURL=index.3c9ec03e.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,9 @@
+"use strict";
+const wall = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+const wallRect = wall.getBoundingClientRect();
+const spiderRect = spider.getBoundingClientRect();
+spider.style.top = `${(wallRect.height - spiderRect.height) / 2}px`;
+spider.style.left = `${(wallRect.width - spiderRect.width) / 2}px`;
+
+//# sourceMappingURL=index.f75de5e1.js.map
